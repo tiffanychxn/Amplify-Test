a0414a7953af5442f3fb0c7040db8c3f9d05d531
Tiff Chan <tiffny.chan@gmail.com>
2022-03-09 21:27:55 -0500

    latest commit

--- src/App.js
+++ src/App.js
@@ -55,7 +55,7 @@ async function createNote() {
 
   return (
     <div className="App">
-      <h1>My Notes App</h1>
+      <h1>My Note(s) App</h1>
       <input
         onChange={e => setFormData({ ...formData, 'name': e.target.value})}
         placeholder="Note name"
